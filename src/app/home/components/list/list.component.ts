226dd3fd2d4f8c1120f0973baaee48bed6b1a98e
Oleksandr Davydiuk <mictopibhe@gmail.com>
2025-01-26 22:52:56 +0100

    added placeholder for draggeable cards

--- src/app/home/components/list/list.component.ts
+++ src/app/home/components/list/list.component.ts
@@ -1,4 +1,12 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output, Renderer2
+} from '@angular/core';
 import {Card} from '../../../core/interfaces/card.interface';
 import {NgForOf} from '@angular/common';
 import {CardComponent} from '../card/card.component';
@@ -24,7 +32,8 @@ import {CardCreationFormComponent} from '../card-creation-form/card-creation-for
 export class ListComponent implements OnInit {
   constructor(private listService: ListService,
               private cardService: CardService,
-              private readonly route: ActivatedRoute) {
+              private readonly route: ActivatedRoute,
+              private cdr: ChangeDetectorRef) {
   }
 
   @Input() listId!: string;
@@ -39,9 +48,16 @@ export class ListComponent implements OnInit {
   isCardCreationEnabled: boolean = false;
   newTitle: string = '';
 
+  placeholder!: HTMLDivElement;
+  draggedElement?: HTMLElement | null = null;
+
+
   ngOnInit(): void {
     this.boardId = this.route.snapshot.paramMap.get('id');
     this.newTitle = this.title;
+    this.cards.sort((a, b) => a.position - b.position);
+    this.placeholder = Array.from(document.getElementsByClassName('placeholder'))[0] as HTMLDivElement;
+    this.placeholder.parentNode?.removeChild(this.placeholder);
   }
 
   removeList() {
@@ -79,5 +95,52 @@ export class ListComponent implements OnInit {
     } else {
       this.isCardCreationEnabled = false;
     }
+  }
+
+  onDragStart(event: DragEvent, card: Card) {
+    this.draggedElement = event.target as HTMLElement;
+    event.dataTransfer!.setData('application/json', JSON.stringify(card));
+    //event.dataTransfer!.dropEffect = "copy"; //поки не зрозуміло, як працює (повинен змінюватись курсор але поки не змінюється)
+    // const target = event.target as HTMLElement;
+    // target.classList.add('dragging');
+  }
+
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    const list = event.currentTarget as HTMLElement;
+
+    const cardComponents = Array.from(list.children).filter(
+      (child) => !child.classList.contains('placeholder')
+    );
+
+    const mouseY = event.clientY;
+    let insertIndex = cardComponents.findIndex((card) => {
+      const cardRect = card.getBoundingClientRect();
+      const cardCenterY = cardRect.top + cardRect.height / 2;
+      return mouseY < cardCenterY;
+    });
+
+    insertIndex = insertIndex === -1 ? cardComponents.length : insertIndex;
+    const targetCard = cardComponents[insertIndex] || null;
+    list.insertBefore(this.placeholder, targetCard);
+  }
+
+  onDragLeave(event: DragEvent) {
+    const list = event.target as HTMLElement;
+    const listRect = list.getBoundingClientRect();
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    if (this.isMouseLeaveDropZone(listRect, mouseX, mouseY)) {
+      // якщо тягнути карту прямо в самий верх листа а потім на інший список - плейсхолдер лишається
+      // @ts-ignore
+      this.placeholder.parentNode.removeChild(this.placeholder);
+    }
+  }
+
+  isMouseLeaveDropZone(listRect: DOMRect, mouseX: number, mouseY: number) {
+    return (listRect.top < mouseY && listRect.bottom > mouseY) ||
+      (listRect.left > mouseX && listRect.right < mouseX);
   }
 }
